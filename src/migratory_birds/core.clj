b53dd86a10f0518efade2d4a0b680cfa36ae7b2d
ccrowe <ccrowe-ld1@linkedin.com>
2019-04-23 08:02:25 -0700

    first attempt at migratory birds

---
+++ src/migratory_birds/core.clj
@@ -0,0 +1,28 @@
+(ns migratory-birds.core
+  (:gen-class))
+
+(defn get-max [input]
+  (reduce max input))
+
+(defn get-most-frequent-count [hm]
+  (get-max (vals hm))
+  )
+
+(defn has-highest-frequency [highest-frequency [k v]]
+  (= highest-frequency v))
+
+(defn migrate-birds [input]
+  (def hm (frequencies input))
+  (def highest-frequency (get-most-frequent-count hm))
+  (def filter-for-highest-frequency (filter (partial has-highest-frequency highest-frequency) hm))
+  (min (keys filter-for-highest-frequency)))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (def input [1 4 4 4 5 3])
+  (migrate-birds input)
+  )
+
+
+; output 4
